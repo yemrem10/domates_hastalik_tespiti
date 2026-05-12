c92548b2c5bda924d8786e777110d9fc10b25ee8
Yunus Emrem <y.emrem.y@gmail.com>
2026-05-12 12:41:05 +0300

    ufak düzeltme

--- uygulama.py
+++ uygulama.py
@@ -4,7 +4,6 @@ from torchvision import transforms, models
 from PIL import Image
 import sqlite3
 import numpy as np
-import cv2 
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
